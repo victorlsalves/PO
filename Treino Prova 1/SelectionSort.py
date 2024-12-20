632b37e5236de864709bad01114417fda4e4687e
victorlsalves <victorlinhares36@gmail.com>
2024-12-20 11:40:55 -0300

    add

--- Treino Prova 1/SelectionSort.py
+++ Treino Prova 1/SelectionSort.py
@@ -6,7 +6,7 @@ def selectionsort(lista):
         for j in range(i + 1, len(lista)):
             if lista[j] < lista[min]:
                 min = j
-            lista[i], lista[min] = lista[min], lista[i]
+        lista[i], lista[min] = lista[min], lista[i]
 
 lista = np.random.randint(1,1000,20)
 print(lista)
